a4a621587dc012e24cabe4133d9e2a7ac035fce2
PrinceYadavOffice <prince.yadav@tothenew.com>
2025-11-26 16:40:41 +0530

    testing Jenkinsfile

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from math_utils import add
 if __name__ == "__main__":
     result = add(5, 7)
     print("testing Jenkins auto build... and deployment...")
+    print("Testing webhook integration...with Jenkins")
     print(f"The sum of 5 and 7 is: {result}")
     
